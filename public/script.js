e48fde9d56399cbc2aa81de698d4ca4da6579480
arunrawat404 <arunrawatar404@gmail.com>
2023-12-23 20:02:27 +0530

    Basic chat app ready

--- public/script.js
+++ public/script.js
@@ -2,6 +2,24 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("Welcome to socket");
     var socket = io();
 
+    let input = document.getElementById("chat_box");
+    let msgList = document.getElementById("msg_list");
+    let send = document.getElementById("send");
+
+    send.addEventListener("click", () => {
+        let msg = input.value;
+        socket.emit("new_msg", {
+            message: msg
+        });
+        input.value = "";
+    });
+
+    socket.on("msg_rcvd", (data) => {
+        let msgLi = document.createElement("li");
+        msgLi.textContent = data.message;
+        msgList.appendChild(msgLi);
+    });
+
 });
 
 
--- server.js
+++ server.js
@@ -8,8 +8,41 @@ const io = new Server(server);
 
 const PORT = 3000;
 
+// to set view engine as ejs
+app.set("view engine", "ejs");
+
+// to serve static files present in public directory
 app.use("/", express.static(__dirname + "/public"));
 
-server.listen(PORT, () => {
+io.on("connection", (socket) => {
+    console.log("A user connected", socket.id);
+    socket.on("disconnect", () => {
+        console.log("User disconnected", socket.id);
+    });
+
+    socket.on("join_room", (data) => {
+        console.log("Joining a room", data.roomid);
+        socket.join(data.roomid);
+    });
+
+    socket.on("new_msg", (data) => {
+        console.log("Received new message", data);
+        // will emit message to particular roomid
+        io.to(data.roomid).emit("msg_rcvd", data);
+    });
+});
+
+app.get("/chat/:roomid/:user", async (req, res) => {
+    res.render("index", {
+        roomid: req.params.roomid,
+        user: req.params.user
+    });
+});
+
+app.get("/group", async (req, res) => {
+    res.render("group");
+});
+
+server.listen(PORT, async () => {
     console.log(`Server is up and running on PORT ${PORT}`);
 });
